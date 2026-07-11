21b22d3c102152558e4997ccf164e14228f1397e
David Heng <hengjinyongd@gmail.com>
2026-07-11 04:10:48 +0000

    Implement display, search, and add student features

--- src/MainConsole.java
+++ src/MainConsole.java
@@ -3,6 +3,12 @@ import java.util.Scanner;
 public class MainConsole{
     public static void main(String[] args){
         Scanner scanner = new Scanner(System.in);
+        StudentManager manager = new StudentManager();
+
+        //Test
+        manager.addStudent("000001", "Alisa Cha", 19, 3.12);
+        manager.addStudent("000002", "Bobby Tan", 21, 2.92);
+
 
         System.out.println("""
         =============================
@@ -13,6 +19,7 @@ public class MainConsole{
         //loop the main menu if choice not equal to 
         while (true) {
             System.out.print("""
+
             1. Display All Student Information
             2. Search Student
             3. Add Student
@@ -24,20 +31,55 @@ public class MainConsole{
             String choice = scanner.nextLine();
 
             switch (choice) {
-                case "1" -> System.out.println("Display all student information");
-                case "2" -> System.out.println("Search student by ID");
-                case "3" -> System.out.println("Add student");
+                case "1" -> manager.displayAllStudent();
+                case "2" -> searchStudent(manager, scanner);
+                case "3" -> addNewStudent(manager, scanner);
                 case "4" -> System.out.println("Remove student by ID");
                 case "5" -> System.out.println("Update Student information by ID");
                 case "6" -> System.out.println("Thanks for using the system");
                 default -> System.out.println("Invalid choice!");
             }
 
-            if (!choice.equals("6")){
+            if (choice.equals("6")){
                 break;
             }
         }
         
         scanner.close();
+    }
+
+    public static void searchStudent(StudentManager manager, Scanner scanner){
+        System.out.print("Enter Student ID: ");
+        String id = scanner.nextLine();
+        
+        Student student = manager.findStudent(id);
+
+        if (student != null) {
+            System.out.println(student.toString());
+        } else {
+            System.out.println("Student Not Found.");
+        }
+    }
+
+    public static void addNewStudent(StudentManager manager, Scanner scanner){
+        System.out.print("Enter ID: ");
+        String id = scanner.nextLine();
+
+        Student checkStudent = manager.findStudent(id);
+        if (checkStudent != null){
+            System.out.println("Student already exist.");
+            return;
+        }
+
+        System.out.print("Enter Name: ");
+        String name = scanner.nextLine();
+
+        System.out.print("Enter age: ");
+        int age = scanner.nextInt();
+
+        System.out.print("Enter CGPA: ");
+        double cgpa = scanner.nextDouble();
+        
+        manager.addStudent(id, name, age, cgpa);
     }
 }
